b719025f72e76de8ad987df782978a021e8c6533
lilaf <lilafian74@proton.me>
2026-03-31 22:20:34 -0500

    remove old ide driver funcs that do nothing

--- src/drivers/ide.c
+++
@@ -1,6 +0,0 @@
-#include <syscom/drivers/ide.h>
-#include <syscom/log.h>
-
-void ide_init(unsigned int bar0, unsigned int bar1, unsigned int bar2, unsigned int bar3, unsigned int bar4) {
-        log("[ide_init] Initializing IDE driver\n");
-}
--- src/include/syscom/drivers/ide.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void ide_init(unsigned int bar0, unsigned int bar1, unsigned int bar2, unsigned int bar3, unsigned int bar4);
